3ea2454aa348a7e47123727fb00286f33a79c401
JakubW <wiechniak.j@gmail.com>
2021-06-07 23:26:48 +0200

    session fix

--- server/components/handleUser.js
+++ server/components/handleUser.js
@@ -1,27 +1,28 @@
 const uniqid = require('uniqid')
 const Room = require('./newRoom').Room
 
-module.exports.handleUser = function (gameRooms, session) {
+module.exports.handleUser = function (gameRooms) {
+    let result = {}
     if (gameRooms.length == 0) {
         let tempRoom = uniqid("room")
         let newRoom = new Room(tempRoom)
         newRoom.addPlayer()
         gameRooms.push(newRoom)
-        session.room = tempRoom
-        session.player = 1
+        result.room = tempRoom
+        result.player = 1
     } else {
         if (gameRooms[gameRooms.length - 1].players < 2) {
             gameRooms[gameRooms.length - 1].addPlayer()
-            session.room = gameRooms[gameRooms.length - 1].getId()
-            session.player = 2
+            result.room = gameRooms[gameRooms.length - 1].getId()
+            result.player = 2
         } else {
             let tempRoom = uniqid("room")
             let newRoom = new Room(tempRoom)
             newRoom.addPlayer()
             gameRooms.push(newRoom)
-            session.room = tempRoom
-            session.player = 1
+            result.room = tempRoom
+            result.player = 1
         }
     }
-    return gameRooms[gameRooms.length - 1].players
+    return result
 }
